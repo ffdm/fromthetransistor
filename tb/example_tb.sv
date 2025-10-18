3ba98028480f838ba29b5517eecb22d333dc5a76
Finn Moore <fdm@umich.edu>
2025-10-18 00:57:44 -0400

    Fix operator preference bug... ;(

--- tb/example_tb.sv
+++ tb/example_tb.sv
@@ -2,6 +2,8 @@ module example_tb;
 
 logic a, b, c;
 
+logic passed = 1'b1;
+
 example e (a, b, c);
 
 initial begin
@@ -9,9 +11,14 @@ initial begin
   repeat(1000) begin
     a = $urandom()[0]; b = $urandom()[0];
     #0.1ns
-    assert(c == a&b) else $display("c: %b, a&b: %b", c, a&b);
+    assert(c == (a&b)) else begin
+      $display("a: %b, b: %b, c: %b, a&b: %b", a, b, c, a&b);
+      passed = 1'b0;
+      break;
+    end
   end
-  $display("\033[0;32mPassed!\033[0m");
+  if (passed) $display("\033[0;32mPassed :)\033[0m");
+  else $display("\033[0;31mFailed :(\033[0m");
 end
 
 endmodule
